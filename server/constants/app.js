21e25b312a86aeca25c210c165ce5677cfd3d6ff
Steven Bapaga <sbapaga@gmail.com>
2018-05-25 13:41:10 +0100

    Make urls constants

--- server/constants/app.js
+++ server/constants/app.js
@@ -1,4 +1,9 @@
 module.exports = {
   ADMIN_ACCOUNT: 'admin account',
   USER_ACCOUNT: 'user account',
+  routes: {
+    ADMIN: '/admin',
+    ADD_USER: '/admin/add-user',
+    CHANGE_PASSWORD: '/change-password',
+  },
 };
