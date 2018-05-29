2f6a3e8083aeaf052cab6a74f8a7638a9614e536
Steven Bapaga <sbapaga@gmail.com>
2018-05-29 12:53:49 +0100

    Remove routes constants

--- server/constants/app.js
+++ server/constants/app.js
@@ -1,9 +1,4 @@
 module.exports = {
   ADMIN_ACCOUNT: 'admin account',
   USER_ACCOUNT: 'user account',
-  routes: {
-    ADMIN: '/admin',
-    ADD_USER: '/admin/add-user',
-    CHANGE_PASSWORD: '/change-password',
-  },
 };
