cc62aa7ea0f724b0b690f7c3c578e62a0c4d298c
Glen Mailer <glen@stainlessed.co.uk>
2018-07-03 19:41:13 +0100

    Also include user details in app insights request logs

--- server/loggers/appInsights.js
+++ server/loggers/appInsights.js
@@ -3,4 +3,13 @@ const config = require('../config');
 
 appInsights.setup(config.appInsightsKey).start();
 
+appInsights.defaultClient.addTelemetryProcessor(attachUserId);
+function attachUserId(envelope, context) {
+  const res = context['http.ServerResponse'];
+  if (res && res.locals && res.locals.user) {
+    // eslint-disable-next-line no-param-reassign
+    envelope.tags['ai.user.authUserId'] = res.locals.user.username;
+  }
+}
+
 module.exports = appInsights;
